fa0ba1f3c19cfe5c788f16b56a014eb53bea8b3c
Aryan741x <128162387+Aryan741x@users.noreply.github.com>
2023-07-24 19:39:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let click;
 let result;
-let win=0;
-let lost=0;
-let draw=0;
+let score=JSON.parse(localStorage.getItem('score'));
+if(score===null){
+  score={
+    win:0,
+    lost:0,
+    draw:0
+  };
+}
 let paper='🤚';
 let rock='✊';
 let sci='✌️';
@@ -11,66 +16,68 @@ function randomNumber(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
 function winners(click,result){
   if(click==1){
     if(result==1){
       document.getElementById('second').innerHTML="Draw!";
-      draw++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.draw++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${rock} VS ${rock}`;
     }
     else if(result==2){
       document.getElementById('second').innerHTML="You Lost.";
-      lost++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.lost++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${rock} VS ${paper}`;
     }
     else{
       document.getElementById('second').innerHTML="You Won.";
-      win++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.win++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${rock} VS ${sci}`;
     }
   }
   else if(click==2){
     if(result==1){
       document.getElementById('second').innerHTML="You Won.";
-      win++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.win++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${paper} VS ${rock}`;
     }
     else if(result==2){
       document.getElementById('second').innerHTML="Draw!";
-      draw++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.draw++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${paper} VS ${paper}`;
     }
     else{
       document.getElementById('second').innerHTML="You Lost.";
-      lost++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.lost++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${paper} VS ${sci}`;
     }
   }
   else{
     if(result==1){
       document.getElementById('second').innerHTML="You Lost.";
-      lost++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.lost++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${sci} VS ${rock}`;
     }
     else if(result==2){
       document.getElementById('second').innerHTML="You Won.";
-      win++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.win++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${sci} VS ${paper}`;
     }
     else{
       document.getElementById('second').innerHTML="Draw!";
-      draw++;
-      document.getElementById('tally').innerHTML=`Win:${win} Lost:${lost} Draw:${draw}`;
+      score.draw++;
+      document.getElementById('tally').innerHTML=`Win:${score.win} Lost:${score.lost} Draw:${score.draw}`;
       document.getElementById('choose').innerHTML=`${sci} VS ${sci}`;
     }
   }
+  localStorage.setItem('score',JSON.stringify(score));
   return 0;
 }
